d987a5fe485ea23f3528ded70dc1338d1260c717
Uygar <r.uygar@gmail.com>
2026-01-09 09:52:04 +0300

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,5 +1,17 @@
-from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+from comfy_api.latest import ComfyExtension
+
+from .nodes import NODES as ARTHA_NODES
+
+class ComfyUIArthaNodesExtension(ComfyExtension):
+      
+    async def get_node_list(self):
+        return [
+            *ARTHA_NODES,
+        ]
+
+async def comfy_entrypoint():
+    
+    return ComfyUIArthaNodesExtension()
+    
 
 WEB_DIRECTORY = "web/js"
-
-__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS', "WEB_DIRECTORY"]
